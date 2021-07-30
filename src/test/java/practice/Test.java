76d4f157c235e4edf6446e4bc49d5eda9598fdad
Azimullahahmadi <88009578+Azimullahahmadi@users.noreply.github.com>
2021-07-30 16:20:08 -0700

    Update Test.java
    i make this changes in github

--- src/test/java/practice/Test.java
+++ src/test/java/practice/Test.java
@@ -6,6 +6,8 @@ public class Test {
 		//i make this changes in local pc
 		System.out.println("this is my GitHub method");
 		
+		// i make this changes in gitHub
+		System.out.println("this changes make in GitHub");
 		
 	}
 }
